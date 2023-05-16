3b8c0f6c5806b62a1eceb6f896720b5de99793a0
Thierry <thierrytajesdossantos@gmail.com>
2023-05-16 10:19:44 -0300

    fix logo size

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Footer } from './components/Footer'
 import { Navbar } from './components/Navbar'
 import { Lexend_Zetta } from 'next/font/google'
+import Image from 'next/image';
 import './global.css'
 
 const zetta = Lexend_Zetta({ subsets: ['latin'] })
@@ -10,7 +11,10 @@ export default function Home() {
     <>
       <Navbar />
       <main>
-        <p className={zetta.className}>Encontre a oportunidade <br />perfeita para VOCÊ.</p>
+        <div className='mt-8 flex flex-col items-center'>
+          <p className={`${zetta.className} text-3xl text-center`}>Encontre a oportunidade <br />perfeita para VOCÊ.</p>
+          <Image className="flex justify-center" src="/hiring.svg" alt="Descrição da imagem" width={665} height={547} />
+        </div>
       </main>
       <Footer />
     </>
