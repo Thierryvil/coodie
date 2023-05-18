ed54404495cbded54008a86c53edd02d40475607
Thierry <thierrytajesdossantos@gmail.com>
2023-05-18 10:39:21 -0300

    add solid svg icons

--- web/src/app/enterprise-timeline/page.tsx
+++ web/src/app/enterprise-timeline/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Logo } from "../components/Logo";
 
 export default function EnterpriseTimeline() {
-    return <h1>Logado</h1>
+    return (
+        <nav className="py-4 px-8">
+            <Logo />
+        </nav>
+    )
 }
