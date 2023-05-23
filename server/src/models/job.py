57f715a7e6a3afbeb731beab184a80250ba4fcf3
Thierry Tajes dos Santos <thierrytajesdossantos@gmail.com>
2023-05-23 07:49:00 -0300

    refact: job model

--- server/src/models/job.py
+++ server/src/models/job.py
@@ -1,53 +1,71 @@
-from typing import List, Optional
+from typing import List
 
 from pydantic import BaseModel, validator
 
+ALLOWED_SENIORITY = {"junior", "pleno", "senior"}
+ALLOWED_LOCATION = {"remoto", "hibrido", "home office"}
+ALLOWED_REGIME = {"clt", "pj"}
+
 
 class Job(BaseModel):
-    id: Optional[int]
     title: str
     description: str
     seniority: List[str]
     location: List[str]
     regime: List[str]
 
+    @classmethod
+    def validate_string_non_blank(cls, value, field_name):
+        if not value.strip():
+            raise ValueError(f"{field_name.capitalize()} must not be blank.")
+        return value
+
     @validator("title")
-    def title_must_not_be_blank(cls, v):
-        if not v.strip():
-            raise ValueError("Title must not be blank.")
-        return v
+    def validate_title(cls, v):
+        return cls.validate_string_non_blank(v, "title")
 
     @validator("description")
-    def description_must_not_be_blank(cls, v):
-        if not v.strip():
-            raise ValueError("Description must not be blank.")
-        return v
+    def validate_description(cls, v):
+        return cls.validate_string_non_blank(v, "description")
 
     @validator("seniority")
     def validate_seniority(cls, v):
         if not v:
-            raise ValueError("Empty seniority")
-        invalid_seniority = set(v) - {"junior", "pleno", "senior"}
+            raise ValueError("Seniority must not be empty.")
+        invalid_seniority = set(v) - ALLOWED_SENIORITY
         if invalid_seniority:
-            raise ValueError(f"Invalid seniority: {invalid_seniority}")
+            invalid_values = ", ".join(invalid_seniority)
+            allowed_values = ", ".join(ALLOWED_SENIORITY)
+            raise ValueError(
+                f"Invalid seniority values: {invalid_values}. "
+                f"Allowed values are: {allowed_values}."
+            )
         return v
 
     @validator("location")
     def validate_location(cls, v):
         if not v:
-            raise ValueError("Empty location")
-
-        invalid_location = set(v) - {"remoto", "hibrido", "presencial"}
+            raise ValueError("Location must not be empty.")
+        invalid_location = set(v) - {"remoto", "hibrido", "home office"}
         if invalid_location:
-            raise ValueError(f"Invalid location: {invalid_location}")
+            invalid_values = ", ".join(invalid_location)
+            allowed_values = ", ".join(ALLOWED_LOCATION)
+            raise ValueError(
+                f"Invalid location values: {invalid_values}. "
+                f"Allowed values are: {allowed_values}."
+            )
         return v
 
     @validator("regime")
     def validate_regime(cls, v):
         if not v:
-            raise ValueError("Empty regime")
-
-        invalid_regime = set(v) - {"clt", "pj"}
+            raise ValueError("Regime must not be empty.")
+        invalid_regime = set(v) - ALLOWED_REGIME
         if invalid_regime:
-            raise ValueError(f"Invalid regime: {invalid_regime}")
+            invalid_values = ", ".join(invalid_regime)
+            allowed_values = ", ".join(ALLOWED_REGIME)
+            raise ValueError(
+                f"Invalid regime values: {invalid_values}. "
+                f"Allowed values are: {allowed_values}."
+            )
         return v
