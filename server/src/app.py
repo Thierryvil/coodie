7532b36990820cb60608997aa86f4b105cce6f48
Thierry Tajes dos Santos <thierrytajesdossantos@gmail.com>
2023-06-20 15:07:04 -0300

    add railway deploy

--- server/src/app.py
+++ server/src/app.py
@@ -1,22 +1,20 @@
+from db.config import create_puc, create_tables
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-from db.config import create_puc, create_tables
 from routes.auth_routes import router as auth_router
 from routes.job_routes import router as job_routes
 from routes.user_router import router as user_router
 
 app = FastAPI()
-app.include_router(job_routes)
-app.include_router(auth_router)
-app.include_router(user_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(job_routes)
+app.include_router(auth_router)
+app.include_router(user_router)
 
 create_tables()
 create_puc()
